809d79789d39b88ec5a58ec1dc9b14f8d47b2e45
dingchao <1109928059@qq.com>
2021-08-27 22:19:12 +0800

    百钱买百鸡

--- python/boluo/basic/06_logic.py
+++ python/boluo/basic/06_logic.py
@@ -8,4 +8,10 @@ for num in range(100, 1000):
     if num == low ** 3 + mid ** 3 + high ** 3:
         print(num)
 
-
+# 百钱买百鸡
+# 公鸡5元一只, 母鸡3元一只, 小鸡一元3只
+for x in range(0, 20):
+    for y in range(0, 33):
+        z = 100 - x - y
+        if 5 * x + 3 * y + z / 3 == 100:
+            print("公鸡: %d, 母鸡: %d, 小鸡: %d" % (x, y, z))
