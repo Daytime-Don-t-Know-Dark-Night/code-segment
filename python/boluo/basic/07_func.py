406db6c5b7f7108adeccaed81c8b5f1f3f10dbe6
dingchao <1109928059@qq.com>
2021-08-30 18:23:58 +0800

    func

--- python/boluo/basic/07_func.py
+++ python/boluo/basic/07_func.py
@@ -1,4 +1,5 @@
 # 输入m, n, 计算C(m,n)
+from random import randint
 m = int(input("m: "))
 n = int(input("n: "))
 fm = 1
@@ -15,4 +16,46 @@ for num in range(1, m - n + 1):
 
 print(fm // fn // fmn)
 
+
 # home branches test
+
+def fac(num):
+    """求阶乘"""
+    res = 1
+    for n in range(1, num+1):
+        res *= n
+    return res
+
+
+m = int(input("m = "))
+n = int(input("n = "))
+
+print(fac(m) // fac(n) // fac(m-n))
+
+
+# python中的函数可以有默认值, 也支持使用可变参数
+
+
+def roll_dice(n=2):
+    """摇色子"""
+    total = 0
+    for _ in range(n):
+        total += randint(1, 6)
+    return total
+
+
+def add(a=0, b=0, c=0):
+    return a+b+c
+
+
+# 如果没有指定参数, 则使用默认参数
+print(roll_dice())
+print(roll_dice(3))
+
+print(add())
+print(add(1))
+print(add(1, 2))
+print(add(1, 2, 3))
+# 传递参数时可以不按照设定的顺序进行传递
+print(add(c=50, a=100, b=200))
+
