4d59b1b20b707c8add98d5e0da1e5942799dc4f7
dingchao <1109928059@qq.com>
2021-09-05 21:57:49 +0800

    list

--- python/boluo/basic/07_string.py
+++ python/boluo/basic/07_string.py
@@ -140,4 +140,42 @@ print(list1)
 # 清空列表
 list1.clear()
 
-# 和字符串一样, 列表也可以做切片操作, 通过切片操作我们可以实现
+# 和字符串一样, 列表也可以做切片操作, 通过切片操作我们可以实现 **********************************************
+fruits = ["grape", "apple", "strawberry", "waxberry"]
+fruits += ["pitaya", "pear", "mango"]
+
+# 列表切片
+fruits2 = fruits[1:4]
+print(fruits2)
+
+# 可以通过完整切片操作来复制列表
+fruits3 = fruits[:]
+print(fruits3)
+fruits4 = fruits[-3:-1]
+print(fruits4)
+
+fruits5 = fruits[::-1]
+print(fruits5)
+
+# 对列表的排序操作
+list1 = ["orange", "apple", "zoo", "internationalization", "blueberry"]
+list2 = sorted(list1)
+# sorted函数返回列表排序后的拷贝不会修改传入的列表1
+# 函数的设计就应该像sorted函数一样, 尽可能不产生副作用
+list3 = sorted(list1, reverse=True)
+list4 = sorted(list1, key=len)
+
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+
+# 给列表对象发出排序消息直接在列表对象上排序
+list1.sort(reverse=True)
+print(list1)
+
+
+
+
+
+
