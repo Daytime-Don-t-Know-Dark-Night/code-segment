b57ac3c7a70449cb8b77b5f22993b675e2d2e20b
dingchao <1109928059@qq.com>
2021-09-08 22:49:16 +0800

    test

--- python/boluo/basic/07_string_test.py
+++ python/boluo/basic/07_string_test.py
@@ -74,3 +74,57 @@ def max2(x):
             m2 = x[index]
 
     return m1, m2
+
+
+# 练习5: 计算指定的年月日是这一年的第几天
+def is_leap_year(year):
+    """
+    判断指定的年份是否为闰年
+    :param year: 年份
+    :return: 闰年返回 True, 平年返回 False
+    """
+
+    return year % 4 == 0 and year % 100 == 0 or year % 400 == 0
+
+
+def which_day(year, month, date):
+    """
+    计算传入的日期是这一年的第几天
+    :param year: 年
+    :param month: 月
+    :param date: 日
+    :return: 第几天
+    """
+    days_of_month = [
+        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+        [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    ][is_leap_year(year)]
+    total = 0
+
+    for index in range(month - 1):
+        total += days_of_month[index]
+    return total + date
+
+
+def main():
+    print(which_day(1980, 11, 28))
+    print(which_day(1981, 12, 31))
+    print(which_day(2018, 1, 1))
+    print(which_day(2016, 3, 1))
+
+
+# 练习6: 打印杨辉三角
+def main():
+    num = int(input("Number of rows: "))
+    yh = [[]] * num
+    for row in range(len(yh)):
+        yh[row] = [None] * (row + 1)
+        for col in range(len(yh[row])):
+            if col == 0 or col == row:
+                yh[row][col] == 1
+            else:
+                yh[row][col] = yh[row - 1][col] + yh[row - 1][col - 1]
+            print(yh[row][col], end="\t")
+        print()
+
+
