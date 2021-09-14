455e7ce47f8442dd16783304315d03313668147e
dingchao <1109928059@qq.com>
2021-09-14 23:02:00 +0800

    pygame

--- python/boluo/basic/10_game.py
+++ python/boluo/basic/10_game.py
@@ -2,10 +2,12 @@
 # Packer(自动将控件填充到合适的位置)
 # Grid(基于网格坐标来摆放控件)
 
+from enum import Enum, unique
+from math import sqrt
+from random import randint
+
 # 如果引用不到换一下解释器, 有可能下载到了别的解释器中
 import pygame
-
-print(pygame.ver)
 
 
 def main():
@@ -15,19 +17,8 @@ def main():
     screen = pygame.display.set_mode((800, 600))
     # 设置当前窗口的标题
     pygame.display.set_caption("大球吃小球")
-    # 设置窗口的背景色
-    screen.fill((255, 255, 255))
-    # 通过指定的文件名加载图像
-    ball_image = pygame.image.load("./res/ball.png")
-    # 在窗口上渲染图像
-    screen.blit(ball_image, (50, 50))
-
-    # 绘制一个圆(参数: 屏幕, 颜色, 圆心位置, 半径, 0表示填充圆)
-    # pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
-
-    # 刷新当前窗口
-    pygame.display.flip()
-
+    # 定义变量来表示小球在屏幕上的距离
+    x, y = 50, 50
     running = True
 
     # 开启一个事件循环处理发生的事件
@@ -36,7 +27,53 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        screen.fill((255, 255, 255))
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), 30, 0)
+        pygame.display.flip()
+
+        # 每隔50毫秒就改变小球的位置再刷新窗口
+        pygame.time.delay(50)
+        x, y = x + 5, y + 5
 
 
 if __name__ == "__main__":
     main()
+
+
+@unique
+class Color(Enum):
+    """颜色"""
+
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GRAY = (242, 242, 242)
+
+    @staticmethod
+    def random_color():
+        """获得随机颜色"""
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        return (r, g, b)
+
+
+class Ball(object):
+    """球"""
+
+    def __init__(self, x, y, radius, sx, sy, color=Color.RED):
+        """初始化方法"""
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.sx = sx
+        self.sy = sy
+        self.color = color
+        self.alive = True
+
+    def move(self, screen):
+        """移动"""
+        self.x += self.sx
+        self.y += self.sy
