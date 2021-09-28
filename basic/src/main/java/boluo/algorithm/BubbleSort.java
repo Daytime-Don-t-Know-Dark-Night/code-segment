2594d653904778c4ba205db8884a09d18b5b12cb
dingchao <1109928059@qq.com>
2021-09-28 18:42:39 +0800

    bubble_sort optimize

--- basic/src/main/java/boluo/algorithm/BubbleSort.java
+++ basic/src/main/java/boluo/algorithm/BubbleSort.java
@@ -11,11 +11,19 @@ public class BubbleSort {
 	}
 
 	public static void bubbleSort(int[] nums) {
+
+		boolean br;
 		for (int i = 0; i < nums.length - 1; i++) {
+			br = false;		// 每次循环之前, 先把br改为false
 			for (int j = 0; j < nums.length - 1 - i; j++) {
 				if (nums[j] > nums[j + 1]) {
 					Swap.swap(nums, j, j + 1);
+					br = true;		// 如果有需要交换的数据, br改为true, 如果br不改为true, 证明已经没有需要交换的数据了
 				}
+			}
+			if (!br) {
+				// 如果一轮循环已经没有需要交换的数据, 则没有再比较下去的必要了
+				return;
 			}
 		}
 	}
