e89735bc7da1aeb64a37a09bc09ca8039c3c3324
dingchao <1109928059@qq.com>
2021-10-11 19:07:34 +0800

    删除排序链表中的重复元素

--- leetcode/src/main/java/com/utils/ListNode.java
+++ leetcode/src/main/java/com/utils/ListNode.java
@@ -2,19 +2,27 @@ package com.utils;
 
 // Definition for singly-linked list.
 public class ListNode {
-	int val;
-	ListNode next;
+	public int val;
+	public ListNode next;
 
-	ListNode() {
+	public ListNode() {
 	}
 
-	ListNode(int val) {
+	public ListNode(int val) {
 		this.val = val;
 	}
 
-	ListNode(int val, ListNode next) {
+	public ListNode(int val, ListNode next) {
 		this.val = val;
 		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "ListNode{" +
+				"val=" + val +
+				", next=" + next +
+				'}';
 	}
 }
 
