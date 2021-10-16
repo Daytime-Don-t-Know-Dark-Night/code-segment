46498f6c6b7622b38da3348fe3869361cd927a31
dingchao <1109928059@qq.com>
2021-10-17 01:50:56 +0800

    路由嵌套

--- vue/hello-vue/src/router/index.js
+++ vue/hello-vue/src/router/index.js
@@ -4,13 +4,21 @@ import Router from "vue-router";
 import Main from "../views/Main";
 import Login from "../views/Login";
 
+import UserList from "../views/user/List";
+import UserProfile from "../views/user/Profile";
+
 Vue.use(Router);
 
 export default new Router({
     routes: [
         {
             path: '/main',
-            component: Main
+            component: Main,
+            // 嵌套子路由
+            children: [
+                {path: '/user/profile', component: UserProfile},
+                {path: '/user/list', component: UserList}
+            ]
         },
         {
             path: '/login',
