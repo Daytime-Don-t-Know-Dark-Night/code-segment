05cd31cb64102f547bba9d31260a3c41431f47de
dingchao <1109928059@qq.com>
2021-10-17 23:07:35 +0800

    vue+element-ui

---
+++ vue/my-demo/src/components/Application.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: '应用管理'
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- vue/my-demo/src/router/index.js
+++ vue/my-demo/src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from '@/components/Home'
+import Config from '@/components/Config'
+import Application from '@/components/Application'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/index',
+            component: Home
+        },
+        {
+            path: '/application',
+            component: Application
+        },
+        {
+            path: '/config',
+            component: Config
+        }
+    ]
 })
