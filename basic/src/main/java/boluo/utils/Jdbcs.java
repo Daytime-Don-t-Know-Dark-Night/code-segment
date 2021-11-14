c7357aa1746ac25f9e84261054a04c71384f5b4c
dingc <1109928059@qq.com>
2021-11-14 17:12:02 +0800

    MySQL插入数据优化

--- basic/src/main/java/boluo/utils/Jdbcs.java
+++ basic/src/main/java/boluo/utils/Jdbcs.java
@@ -36,6 +36,14 @@ public class Jdbcs {
 		void accept(Statement stat, boolean exists) throws Exception;
 	}
 
+	public static JdbcOptionsInWrite options(String uri, String table) {
+		return options(uri, table, null, null);
+	}
+
+	public static JdbcOptionsInWrite options(Iterable<String> cols, String opt) {
+		return options(null, null, cols, opt);
+	}
+
 	public static JdbcOptionsInWrite options(String uri, String table, String... cols) {
 		return options(uri, table, Arrays.asList(cols), null);
 	}
@@ -46,7 +54,7 @@ public class Jdbcs {
 		if (uri.contains("postgresql")) {
 			props.put(JDBCOptions.JDBC_DRIVER_CLASS(), "org.postgresql.Driver");
 		} else {
-			props.put(JDBCOptions.JDBC_DRIVER_CLASS(), "com.mysql.jdbc.Driver");
+			props.put(JDBCOptions.JDBC_DRIVER_CLASS(), "com.mysql.cj.jdbc.Driver");
 		}
 		if (!Strings.isNullOrEmpty(uri)) {
 			props.put(JDBCOptions.JDBC_URL(), uri);
@@ -62,6 +70,41 @@ public class Jdbcs {
 		}
 		Seq<Tuple2<String, String>> tuple2Seq = JavaConverters.mapAsScalaMapConverter(props).asScala().toSeq();
 		return new JdbcOptionsInWrite((scala.collection.immutable.Map<String, String>) Map$.MODULE$.apply(tuple2Seq));
+	}
+
+	public static String onDuplicateUpdate(StructType schema, String sql2, String... exclude) {
+		Set<String> excludeSet = Arrays.stream(exclude).collect(Collectors.toSet());
+
+		Iterable<String> cols1 = Splitter.on(",").trimResults().omitEmptyStrings().split(sql2);
+		Pattern pat = Pattern.compile("([^=]+)=[^=]+$");
+		Streams.stream(cols1).forEach(i -> {
+			Matcher matcher = pat.matcher(i);
+			Preconditions.checkArgument(matcher.matches(), "format error in " + i);
+			excludeSet.add(CharMatcher.anyOf("`").trimFrom(matcher.group(1)));
+		});
+
+		List<String> cols2 = Arrays.stream(schema.fields())
+				.filter(i -> !excludeSet.contains(i.name()))
+				.map(i -> String.format("`%s`=values(`%s`)", i.name(), i.name()))
+				.collect(Collectors.toList());
+		Preconditions.checkArgument(Iterables.size(cols1) + cols2.size() + exclude.length == schema.size());
+		String cols = Joiner.on(",").join(Iterables.concat(cols1, cols2));
+		return "on duplicate key update " + cols;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void execute(String uri, String sql, String... sql2) throws SQLException {
+		JdbcOptionsInWrite options_ = options(uri, "t");
+		try (Connection conn = JdbcUtils.createConnectionFactory(options_).apply()) {
+			conn.setAutoCommit(false);
+			try (Statement statement = conn.createStatement()) {
+				statement.execute(sql);
+				for (String s : sql2) {
+					statement.execute(s);
+				}
+			}
+			conn.commit();
+		}
 	}
 
 }
