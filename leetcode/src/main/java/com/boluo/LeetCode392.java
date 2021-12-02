a31a9aa4b314b4c43c7dcba55e0b2e76ab653a6a
dingchao <1109928059@qq.com>
2021-12-02 18:30:13 +0800

    判断子序列

--- leetcode/src/main/java/com/boluo/LeetCode392.java
+++ leetcode/src/main/java/com/boluo/LeetCode392.java
@@ -1,5 +1,10 @@
 package com.boluo;
 
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
  * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
@@ -20,11 +25,26 @@ package com.boluo;
 public class LeetCode392 {
 
 	public static void main(String[] args) {
-
+		String s = "abc";
+		String t = "ahbgdc";
+		Assert.assertTrue(isSubsequence(s, t));
 	}
 
+	// 单次遍历
 	public static boolean isSubsequence(String s, String t) {
-		// TODO 判断子序列 简单
+		// 创建s的数组array, 遍历t, 如果array[0] = t[i], 就把array[0]删除, 最后如果数组为空, 则为true
+		List<Character> sc = new String(s.toCharArray()).chars().mapToObj(i -> (char) i).collect(Collectors.toList());
+		for (char c : t.toCharArray()) {
+			if (!sc.isEmpty() && sc.get(0) == c) {
+				sc.remove(0);
+			}
+		}
+		return sc.isEmpty();
+	}
+
+	// 双指针解法
+	public static boolean isSubsequence2(String s, String t) {
+
 		throw new UnsupportedOperationException();
 	}
 }
