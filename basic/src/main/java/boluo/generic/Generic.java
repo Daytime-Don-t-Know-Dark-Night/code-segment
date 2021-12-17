b429b606db88f6585669a3213e42193899530bc0
dingchao <1109928059@qq.com>
2021-12-17 18:36:33 +0800

    Java Generic class

--- basic/src/main/java/boluo/generic/Generic.java
+++ basic/src/main/java/boluo/generic/Generic.java
@@ -32,5 +32,52 @@ public class Generic {
 		}
 	}
 
+	// 泛型有三种使用方式: 泛型类, 泛型接口, 泛型方法
+	// 一个最普通的泛型类:
+	public class Generic1<T> {    // 这里T可以随便写为任意标识, 常见的如T, E, K, V等
+		// key这个成员变量的类型为T, T的类型由外部指定
+		private T key;
+
+		public Generic1(T key) {        // 泛型构造方法形参key的类型也为T, T的类型由外部指定
+			this.key = key;
+		}
+
+		public T getKey() {        // 泛型方法getKey的返回值类型为T, T的类型由外部指定
+			return key;
+		}
+	}
+
+	@Test
+	public void func2() {
+		// 泛型的类型只能是类类型(包括自定义类), 不能是基本类型
+		// 传入的实参类型与泛型的类型参数类型相同, 即为Integer, String
+		Generic1<Integer> genericInteger = new Generic1<>(123456);
+		Generic1<String> genericString = new Generic1<>("key_value");
+
+		logger.info("泛型测试, key is {}", genericInteger.getKey());
+		logger.info("泛型测试, key is {}", genericString.getKey());
+	}
 
 }
+
+// 泛型接口
+interface Generator<T> {
+	public T next();
+}
+
+/**
+ * 未传入泛型实参时, 与泛型类的定义相同, 在声明类的时候, 需要将泛型的声明一并加入到类中
+ *
+ * @param <T>
+ */
+class FruitGenerator<T> implements Generator<T> {
+
+	private T[] fruits;
+
+	@Override
+	public T next() {
+		return null;
+	}
+}
+
+
