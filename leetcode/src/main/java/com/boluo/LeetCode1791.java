2a37d48bb886a54987e4827681598efddd1ff04d
dingchao <1109928059@qq.com>
2022-02-22 17:42:34 +0800

    找出星型图的中心节点

--- leetcode/src/main/java/com/boluo/LeetCode1791.java
+++ leetcode/src/main/java/com/boluo/LeetCode1791.java
@@ -8,8 +8,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- *
- *
+ * 找出星型图的中心节点
+ * <p>
  * 有一个无向的星型图，由n个编号从1到n的节点组成。星型图有一个中心节点，并且恰有 n - 1 条边将中心节点与其他每个节点连接起来。
  * 给你一个二维整数数组 edges ，其中 edges[i] = [ui, vi] 表示在节点 ui 和 vi 之间存在一条边。请你找出并返回 edges 所表示星型图的中心节点。
  * <p>
