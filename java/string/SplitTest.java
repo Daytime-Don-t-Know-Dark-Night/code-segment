d3018d4383dd2efbd46f6736d9d109d6c24184ab
dingchao <1109928059@qq.com>
2023-01-14 21:52:10 +0800

    Split保留分割符

--- java/string/SplitTest.java
+++ java/string/SplitTest.java
@@ -13,7 +13,7 @@ public class SplitTest {
 
     public static void main(String[] args) {
         String sql = "SELECT * FROM table_a WHERE id = '001' AND name = 'c'";
-        // 按照关键字将SQL拆分开
+        // 按照关键字将SQL拆分开, 保留分隔符
         String[] sqls = sql.split("(?<=WHERE)|(?=WHERE)|(?<=AND)|(?=AND)");
         Arrays.stream(sqls).forEach(System.out::println);
     }
