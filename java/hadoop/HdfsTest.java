742c10d9eb6384175369d2e1784429c7821e4e9d
dingchao <1109928059@qq.com>
2023-01-19 22:06:21 +0800

    HDFS操作

--- java/hadoop/HdfsTest.java
+++ java/hadoop/HdfsTest.java
@@ -15,12 +15,11 @@ import java.io.IOException;
  * @date 2022/10/27 20:11
  * @desc
  */
-public class HDFSUTest {
+public class HdfsTest {
 
     public static void main(String[] args) throws IOException {
 
         SparkSession.builder().master("local[*]").appName("SparkDemo").getOrCreate();
-
         // String batchId = "OH20221027";
         // String tmpPath = "file:///C:/Users/chao/IdeaProjects/parent/doc/tmp/";
         // String targetPath = "file:///C:/Users/chao/IdeaProjects/parent/doc/target/";
@@ -36,7 +35,6 @@ public class HDFSUTest {
 
     // 将tmp路径下的文件转移到target路径, 并且重命名, 删除原文件和原目录
     private static void renameMove(String sourcePath, String targetPath, String batchId) throws IOException {
-
         Path path = new Path(sourcePath);
         FileSystem fs = FileSystem.get(SparkSession.active().sparkContext().hadoopConfiguration());
         FileStatus[] fsParentFiles = fs.listStatus(path);
@@ -50,12 +48,10 @@ public class HDFSUTest {
             fs.rename(new Path(sourcePath + currentName), new Path(newName));
             fs.delete(new Path(sourcePath), true);
         }
-
     }
 
     // 创建文件
     public static void createFile(String path, byte[] contents) throws IOException {
-
         FileSystem fs = FileSystem.get(SparkSession.active().sparkContext().hadoopConfiguration());
         Path dstPath = new Path(path);
 
@@ -69,7 +65,6 @@ public class HDFSUTest {
 
     // 追加文件, 会报错 java.lang.UnsupportedOperationException: Append is not supported by ChecksumFileSystem
     private static void append(String source) throws IOException {
-
         Path path = new Path(source);
         FileSystem fs = FileSystem.get(SparkSession.active().sparkContext().hadoopConfiguration());
 
@@ -80,7 +75,6 @@ public class HDFSUTest {
         fs.setConf(conf);
 
         FSDataOutputStream fsAppend = fs.append(path);
-
         String s = "this is append content";
         fsAppend.writeBytes(s);
     }
