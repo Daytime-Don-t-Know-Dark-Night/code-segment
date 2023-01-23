10ca3952ed747921847b92e1049d19d541b3f821
dingchao <1109928059@qq.com>
2023-01-23 16:02:27 +0800

    DStream有状态操作: window

--- scala/spark/streaming/DStreamStatus.scala
+++ scala/spark/streaming/DStreamStatus.scala
@@ -1,7 +1,7 @@
 package spark.streaming
 
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.ReceiverInputDStream
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
@@ -11,16 +11,17 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
  */
 object DStreamStatus {
 
-    // nc -lp 9999
+    // nc -lp 9999, 在使用有状态操作时, 需要设定检查点目录
     val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming")
     val sc = new StreamingContext(sparkConf, Seconds(3))
+    sc.checkpoint("checkpoint")
     val datas: ReceiverInputDStream[String] = sc.socketTextStream("localhost", 9999)
 
     def main(args: Array[String]): Unit = {
-        func2()
+        func1()
     }
 
-    def func1(): Unit = {
+    def func0(): Unit = {
 
         // 无状态数据操作, 只对当前的采集周期内的数据进行处理, 例如输入12个a, 会分一次或多次分别统计 (a, 5) (a, 7)
         val wordToOne = datas.map((_, 1))
@@ -31,10 +32,9 @@ object DStreamStatus {
         sc.awaitTermination()
     }
 
-    def func2(): Unit = {
+    // 1. 有状态操作: updateStateByKey
+    def func1(): Unit = {
 
-        // 在使用有状态操作时, 需要设定检查点目录
-        sc.checkpoint("checkpoint")
         val wordToOne = datas.map((_, 1))
 
         // updateStateByKey(): 根据key对数据的状态进行更新
@@ -44,6 +44,22 @@ object DStreamStatus {
         })
 
         state.print()
+        sc.start()
+        sc.awaitTermination()
+    }
+
+    // 2. 有状态操作: window
+    def func2(): Unit = {
+
+        val wordToOne = datas.map((_, 1))
+
+        // 窗口的范围应该是采集周期的整数倍
+        // 不设置步长的话有可能会出现重复数据, 将步长设置为和窗口范围一样大, 就不会再出现重复数据
+        // val windowDs: DStream[(String, Int)] = wordToOne.window(Seconds(6))
+        val windowDs: DStream[(String, Int)] = wordToOne.window(Seconds(6), Seconds(6))
+        val wordToCount = windowDs.reduceByKey(_+_)
+        wordToCount.print()
+
         sc.start()
         sc.awaitTermination()
     }
