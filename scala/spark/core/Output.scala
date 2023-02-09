fbc975e6156f6c55b9adac3e841720503137e6e6
dingchao <1109928059@qq.com>
2023-02-09 22:50:28 +0800

    rdd output

--- scala/spark/core/Output.scala
+++ scala/spark/core/Output.scala
@@ -9,16 +9,30 @@ import org.apache.spark.sql.{Dataset, Row, SparkSession}
  */
 object Output {
 
+    val baseOutputPath = "C:\\Users\\chao\\IdeaProjects\\code-segment\\doc\\spark\\output"
+
     def main(args: Array[String]): Unit = {
-
         val ds = RDD.getDs
-        ds.show(false)
-
-        func1(ds)
+        func2(ds)
     }
 
+    // 1. 默认存储为parquet文件
     def func1(ds: Dataset[Row]): Unit = {
+        ds.show(false)
+        ds.coalesce(1)
+            .write
+            .save(baseOutputPath + "/parquet")
+    }
 
+    // 2. 存储为csv格式
+    def func2(ds: Dataset[Row]): Unit = {
+        ds.show(false)
+        ds.coalesce(1)
+            .write
+            .option("inferSchema", "false")
+            .option("header", "true")
+            .option("charset", "UTF-8")
+            .csv(baseOutputPath + "/csv")
     }
 
 
