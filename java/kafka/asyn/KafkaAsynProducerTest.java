8130349ddfa093ebd952ec06d1f0fe62027f32e4
dingc <1109928059@qq.com>
2023-07-23 20:59:32 +0800

    异步发送消息

--- java/kafka/asyn/KafkaAsynProducerTest.java
+++ java/kafka/asyn/KafkaAsynProducerTest.java
@@ -14,6 +14,7 @@ import java.util.Properties;
  */
 public class KafkaAsynProducerTest {
 
+    // https://blog.csdn.net/u011294519/article/details/104723784
     private static KafkaProducer<String, String> producer = null;
 
     public static void main(String[] args) {
