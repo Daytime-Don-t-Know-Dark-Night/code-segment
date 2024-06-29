ec9729acefbb4d7cad2420293ec3c2e20c8d49f5
dingc <1109928059@qq.com>
2024-06-29 16:19:06 +0800

    update

---
+++ java/spark/sharing/Demo01.java
@@ -0,0 +1,54 @@
+package spark.sharing;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.types.StructType;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+import static org.apache.spark.sql.functions.*;
+
+/**
+ * @author chao
+ * @datetime 2024-06-23 20:11
+ * @description
+ */
+public class Demo01 {
+
+    private static Dataset<Row> ds;
+
+    static {
+        SparkSession spark = SparkSession.builder().master("local[*]").getOrCreate();
+        StructType scheme = new StructType()
+                .add("id", "int")
+                .add("name", "string")
+                .add("age", "int")
+                .add("email", "string")
+                .add("phone", "string")
+                .add("image", "string");
+
+        Row row1 = RowFactory.create(1, "dingc", 20, "cding@state.com", "16631086282", "img/dingc");
+        Row row2 = RowFactory.create(2, "boluo", 20, "boluo@state.com", "17731086282", "img/boluo");
+        Row row3 = RowFactory.create(3, "qidai", 30, "qidai@state.com", "18831086282", "img/qidai");
+        ds = spark.createDataFrame(ImmutableList.of(row1, row2, row3), scheme);
+    }
+
+
+    public static void main(String[] args) {
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        System.out.println(now.format(formatter));
+
+
+        SparkSession.active().sparkContext().setJobDescription("计算age");
+        ds.groupBy("age").agg(count("name")).show(false);
+        Uninterruptibles.sleepUninterruptibly(Duration.ofDays(1));
+    }
+
+
+}
---
+++ java/utils/Animal.java
@@ -0,0 +1,22 @@
+package utils;
+
+/**
+ * @author chao
+ * @datetime 2024-03-15 23:33
+ * @description
+ */
+public class Animal {
+
+    public String str1 = "abc1";
+    private String str2 = "abc2";
+
+    public void func1(){
+        System.out.println("func1()...");
+    }
+
+    private void func2(){
+
+    }
+
+
+}
