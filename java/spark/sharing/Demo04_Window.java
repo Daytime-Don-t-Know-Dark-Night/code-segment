66bc9bcf91dbfd479e09ed43d4ecc5333e921f72
dingc <1109928059@qq.com>
2024-07-11 00:18:19 +0800

    update

--- java/spark/sharing/Demo04_Window.java
+++ java/spark/sharing/Demo04_Window.java
@@ -13,6 +13,7 @@ import org.apache.spark.util.CollectionAccumulator;
 import org.apache.spark.util.LongAccumulator;
 import scala.Option;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,27 +27,37 @@ import static org.apache.spark.sql.functions.*;
 public class Demo04_Window {
 
 
-
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder().master("local[*]").getOrCreate();
         StructType scheme = new StructType()
                 .add("id", "int")
                 .add("name", "string")
-                .add("day", "int")
+                .add("date", "date")
                 .add("score", "int");
 
-        Row row1 = RowFactory.create(1, "dingc", 1, 75);
-        Row row2 = RowFactory.create(2, "dingc", 2, 90);
-        Row row3 = RowFactory.create(3, "dingc", 3, 80);
-        Row row4 = RowFactory.create(4, "dingc", 4, 90);
-        Row row5 = RowFactory.create(5, "dingc", 5, 60);
-        Row row6 = RowFactory.create(6, "boluo", 1, 60);
-        Row row7 = RowFactory.create(7, "boluo", 2, 90);
-        Row row8 = RowFactory.create(8, "boluo", 3, 75);
-        Row row9 = RowFactory.create(9, "boluo", 4, 80);
-        Row row10 = RowFactory.create(10, "boluo", 5, 70);
+        Row row1 = RowFactory.create(1, "dingc", LocalDate.of(2024, 7, 1), 75);
+        Row row2 = RowFactory.create(2, "dingc", LocalDate.of(2024, 7, 2), 90);
+        Row row3 = RowFactory.create(3, "dingc", LocalDate.of(2024, 7, 3), 80);
+        Row row4 = RowFactory.create(4, "dingc", LocalDate.of(2024, 7, 4), 90);
+        Row row5 = RowFactory.create(5, "dingc", LocalDate.of(2024, 7, 5), 60);
+        Row row6 = RowFactory.create(6, "boluo", LocalDate.of(2024, 7, 1), 60);
+        Row row7 = RowFactory.create(7, "boluo", LocalDate.of(2024, 7, 2), 90);
+        Row row8 = RowFactory.create(8, "boluo", LocalDate.of(2024, 7, 3), 75);
+        Row row9 = RowFactory.create(9, "boluo", LocalDate.of(2024, 7, 4), 80);
+        Row row10 = RowFactory.create(10, "boluo", LocalDate.of(2024, 7, 5), 70);
 
         Dataset<Row> ds = spark.createDataFrame(ImmutableList.of(row1, row2, row3, row4, row5, row6, row7, row8, row9, row10), scheme);
-        ds.show(false);
+        // ds.show(false);
+
+        // ds.groupBy("name").agg(sum("score")).show(false);
+
+        // ds.withColumn("sum", expr("sum(score) over(partition by name order by date)")).show(false);
+
+        ds.withColumn("sum", expr("sum(score) over(partition by name order by date rows between unbounded preceding and current row)"))
+                .show(false);
+
+        ds.withColumn("sum", expr("sum(score) over(partition by name order by date rows between 2 preceding and current row)"))
+                .show(false);
+
     }
 }
