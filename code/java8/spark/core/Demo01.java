36d4f9938fa3229aea93a255aae687e6e113a5c4
dingc <1109928059@qq.com>
2024-09-02 00:19:02 +0800

    update

--- code/java8/spark/core/Demo01.java
+++ code/java8/spark/core/Demo01.java
@@ -13,7 +13,7 @@ import static org.apache.spark.sql.functions.*;
  * @Date 2022-08-27 10:37
  * @Description
  */
-public class Demo1 {
+public class Demo01 {
 
     public static void main(String[] args) {
 
