80c75112e9b22adf0cfd6496ac6886c6ae92d619
dingc <1109928059@qq.com>
2025-01-22 21:30:21 +0800

    structured streaming

---
+++ code/java8/spark/structured/SparkApplication.java
@@ -0,0 +1,16 @@
+package java8.spark.structured;
+
+/**
+ * @author chao
+ * @datetime 2025-01-22 21:29
+ * @description
+ */
+public class SparkApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
