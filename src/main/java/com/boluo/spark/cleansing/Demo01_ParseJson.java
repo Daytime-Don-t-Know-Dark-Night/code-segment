250ac0058c6cebf80aa129934bf3188dc1734d44
dingc <1109928059@qq.com>
2025-03-23 19:38:14 +0800

    update spark

---
+++ src/main/java/com/boluo/spark/cleansing/Demo01_ParseJson.java
@@ -0,0 +1,16 @@
+package com.boluo.spark.cleansing;
+
+/**
+ * @author chao
+ * @datetime 2025-03-23 19:23
+ * @description
+ */
+public class Demo01_ParseJson {
+
+    // todo
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/com/boluo/spark/cleansing/Demo01_ParseJsonTest.java
@@ -0,0 +1,17 @@
+package com.boluo.spark.cleansing;
+
+import org.junit.Test;
+
+/**
+ * @author chao
+ * @datetime 2025-03-23 19:31
+ * @description
+ */
+public class Demo01_ParseJsonTest {
+
+    @Test
+    public void func1() {
+        // todo
+    }
+
+}
